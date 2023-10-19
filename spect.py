a985559b56fed6606a98960da5bf7dd0b5ce7a55
Pritesh Agarwal <pritesh0@pop-os.localdomain>
2023-10-19 18:27:05 +0530

    peak identification

--- spect.py
+++ spect.py
@@ -1,24 +1,40 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
 
-image = cv2.imread('t3.png', cv2.IMREAD_GRAYSCALE)
+
+image = cv2.imread('t2.png', cv2.IMREAD_GRAYSCALE)
 
 pixel_positions = np.arange(0, image.shape[1])
+#print(pixel_positions)
+
 calibration_factor=2
 wavelengths = pixel_positions * calibration_factor
 
 # Extract the spectrum
 spectrum = np.sum(image, axis=0)  # Sum along columns
-#print(spectrum)
+#print(len(spectrum),len(pixel_positions))
+
+#pair=[]
+#for i in range(len(spectrum)):
+#    pair.append((spectrum[i],wavelengths[i]))
+#pair.sort(key= lambda x:x[0], reverse=1)
+
+#for i in range(10):
+#    print(pair[i])
+peaks=find_peaks(spectrum,height=10,threshold=5,distance=5)
+print(wavelengths[peaks[0]])
+
 
 known=spectrum
 
 fft_known=np.fft.fft(known)
 fft_spect=np.fft.fft(spectrum)
-
+#print(fft_spect)
 ccr=np.fft.ifft(fft_known*np.conj(fft_spect))
-
+#print(ccr)
+#plt.plot(ccr)
 
 # Plot the spectrum
 plt.figure(figsize=(10, 5))
